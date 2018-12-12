1a5122daa9efe73062ec8daad977b08d32220bd5
JC Chung <jcchung@chicagobooth.edu>
2018-12-12 02:07:22 +0000

    Dishes.index

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -34,7 +34,7 @@ class BookmarksController < ApplicationController
     @bookmark.dish_id = params.fetch("dish_id")
     @bookmark.venue_id = params.fetch("venue_id")
     @bookmark.user_id = params.fetch("user_id")
-    @bookmark.notes = params.fetch("notes")
+    #@bookmark.notes = params.fetch("notes")
 
     save_status = @bookmark.save
 
